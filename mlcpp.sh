1aecc26a09c052c7dba0d1fc48a6d0dbe426fbe4
Motyak <m0tyak_@live.fr>
2025-07-05 16:33:59 +0200

    cpp args OK

--- mlcpp.sh
+++ mlcpp.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -o errexit
 set -o pipefail
+shopt -s expand_aliases
 
-FILE="$1"
+alias cpp='cpp -w'
 
-[ "${FILE##*.}" == "mlp" ] || {
+ARGS="${@:1:$#-1}"
+FILE="${@:(-1)}"
+
+[[ "$FILE" =~ ".mlp"$ ]] || {
     >&2 echo "Invalid file extension: \`${FILE##*.}\`"
     exit 1
 }
 
 cat /dev/null > "${FILE%.mlp}.ml"
-cpp -w "$FILE" \
+cpp $ARGS "$FILE" \
     | perl -0pe 's/^package main\n.*?\n# /# /gms' \
     | perl -pe 's/^package [^ ]+$//gm' \
     | perl -pe 's/^# \d.*\n//gm' \
